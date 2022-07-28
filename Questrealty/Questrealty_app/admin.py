5e1a6b584a56542b36a4e948dc7183b109e21095
Catherine Muthoni <c.muthoni@alustudent.com>
2022-07-28 20:26:21 +0300

    Revert "Merge Bisi's work to main"

--- Questrealty/Questrealty_app/admin.py
+++ Questrealty/Questrealty_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Payment, Review, Message, Room
 
 # Register your models here.
+admin.site.register(Payment)
+admin.site.register(Message)
+admin.site.register(Room)
+admin.site.register(Review)
